29e2db95c8256a1b7c39c7509e684caf4c714730
Paul Donald <newtwen+github@gmail.com>
2024-09-17 17:42:54 +0200

    Archive 2

---
+++ scripts/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+set -e
+
+MODE="$1"
+ARCH="$2"
+
+if ! hash xcpretty 2> /dev/null; then
+  echo "xcpretty not installed. Try gem install xcpretty"
+  trap - EXIT
+  exit 1
+fi
+
+if [ "$MODE" = "build" ]; then
+  echo "Running Build"
+  set -o pipefail && xcodebuild install -project macSup2Srt.xcodeproj -scheme "macSup2Srt" -destination "platform=macOS,arch=$ARCH" install CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO DSTROOT="/Users/runner/work/macSup2Srt/macSup2Srt/Release" | xcpretty -c
+  success="1"
+fi
+
+if [ "$success" = "1" ]; then
+trap - EXIT
+exit 0
+fi
+
+echo "Unrecognized mode '$MODE'."
